e6f3c60f5670838f4d51116023de6b8232b97353
yubrajgithub <127959581+yubrajgithub@users.noreply.github.com>
2023-07-31 19:11:13 +0545

    Add files via upload

---
+++ basic.php
@@ -0,0 +1,7 @@
+<?php
+echo "hellow world";
+//variable
+
+$num=10;//number data type
+echo $num;
+echo "<br> Number : $num=10";
---
+++ practice.php
@@ -0,0 +1,50 @@
+
+ Q1. LIst the type of comment.(Documentation) 
+ Q2. Demonstrate Array types with example. (Data structure Mgmt.)
+ Q3. Describe data type in php, liST THE TYPES. (Data TYpe)
+ Q4. wAP TO demostrate data andtime in php. (Built-in Library/Object)
+ 
+
+ Q5. ...(POP/OOP)
+    -pop: process Oriented Programming
+    -OOp: Object Oriented Programming
+
+    programming paradigm
+       -Imperative
+       -Declarative
+
+    programming Architecture: MVC,3-tier etc.......
+         MVC: Model, view, controller
+         3-tier: presenation layer, busin
+
+$Var_name;  // variable declaration in PHP
+$Var_name = Your name;  // variable defination in PHP
+
+Data type in PHP
+   -Dyanamic Typing / Run time (non static)
+   $name = "your name"; // string
+   $age = 24;  //number
+   $status =true; // bolean
+
+   etc ......(which type manipulate on interpretation by bowser)
+
+
+Tasks:
+-Mention fours Question
+-Programing paradigm
+-programming Architectur
+-OOP Terminology
+      -class (blueprint of instance)
+      -Object (Instant of class)
+      -Inheritance(single and multiple)
+      -Abstract
+      -Interface and implemented
+      -Polymorphism (Overriding and overloading)
+      -Acces Modifier (Private,procted, public)
+      -keyword:static, final , self, parent
+      -Encapsulation(i.e : setter and getter)
+      -Members
+      -constructor and destructor.....
+
+
+
